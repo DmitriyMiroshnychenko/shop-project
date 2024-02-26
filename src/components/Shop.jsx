86f3b942a756a6a422d3de751efbb38f50a28275
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-02-26 21:13:19 +0200

    added function to shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,10 +1,21 @@
 import { useState, useEffect } from 'react';
+import { API_KEY, API_URL } from '../config';
 
 function Shop() {
   const [goods, setGoods] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  useEffect(function getGoods() {}, []);
+  useEffect(function getGoods() {
+    fetch(API_URL, {
+      headers: {
+        Authorization: API_KEY,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setGoods(data.featurted);
+      });
+  }, []);
 
   return <main className="container content">Shop</main>;
 }
