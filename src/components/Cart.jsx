4e28821ee80235e1266dfbbdbdf71ef19092d4cc
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-03-11 21:38:24 +0200

    added new component

---
+++ src/components/Cart.jsx
@@ -0,0 +1,11 @@
+function Cart(props) {
+  const { quantity = 0 } = props;
+  return (
+    <div className="cart">
+      <i className="material-icons">local_grocery_store</i>
+      {quantity ? <span className="cart-quantity">{quantity}</span> : null}
+    </div>
+  );
+}
+
+export { Cart };
