abffd7756bdcc551bef0b59865627deffe0f2c79
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-01 21:51:21 +0300

    added function addToBasket in Shop

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -10,6 +10,30 @@ function Shop() {
   const [loading, setLoading] = useState(true);
   const [order, setOrder] = useState([]);
 
+  const addToBasket = (item) => {
+    const itemIndex = order.findIndex((orderItem) => orderItem.id === item.id);
+
+    if (itemIndex < 0) {
+      const newItem = {
+        ...item,
+        quantity: 1,
+      };
+      setOrder([...order, newItem]);
+    } else {
+      const newOrder = order.map((orderItem, index) => {
+        if (index === itemIndex) {
+          return {
+            ...orderItem,
+            quantity: orderItem.quntity + 1,
+          };
+        } else {
+          return item;
+        }
+      });
+      setOrder(newOrder);
+    }
+  };
+
   useEffect(function getGoods() {
     fetch(API_URL, {
       headers: {
@@ -26,7 +50,11 @@ function Shop() {
   return (
     <main className="container content">
       <Cart quantity={order.length} />
-      {loading ? <Preloader /> : <GoodsList goods={goods} />}
+      {loading ? (
+        <Preloader />
+      ) : (
+        <GoodsList goods={goods} addToBasket={addToBasket} />
+      )}
     </main>
   );
 }
