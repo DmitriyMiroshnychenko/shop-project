d8ffc52e6d6108f88889e8e3f4a1b05e20460428
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-02 22:41:09 +0300

    added new component BasketList

---
+++ src/components/BasketList.jsx
@@ -0,0 +1,15 @@
+function BasketList(props) {
+  const { order } = props;
+
+  return (
+    <ul className="collection">
+      <li className="collection-item active">Корзина</li>
+      <li className="collection-item">Alvin</li>
+      <li className="collection-item">Alvin</li>
+      <li className="collection-item">Alvin</li>
+      <li className="collection-item active">Общая стоимость:</li>
+    </ul>
+  );
+}
+
+export { BasketList };
