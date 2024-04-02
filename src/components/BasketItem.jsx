e080bad97865e739c2750325379ed73883b0fc38
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-02 22:58:12 +0300

    Update BasketItem

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -1,3 +1,14 @@
-function BasketItem(props) {}
+function BasketItem(props) {
+  const { id, name, price, quantity } = props;
+
+  return (
+    <li className="collection-item">
+      {name} x{quantity} = {price}
+      <span class="secondary-content">
+        <i class="material-icons">close</i>
+      </span>
+    </li>
+  );
+}
 
 export { BasketItem };
