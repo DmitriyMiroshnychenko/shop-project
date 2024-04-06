f2d57f6b053a7104d4e77bc945070ead7ff17be5
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-06 14:37:41 +0300

    added new function to BasketList

--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -3,6 +3,10 @@ import { BasketItem } from './BasketItem';
 function BasketList(props) {
   const { order = [], handleBasketShow = Function.prototype } = props;
 
+  const totalPrice = order.reduce((sum, el) => {
+    return sum + el.price * el.quantity;
+  }, 0);
+
   return (
     <ul className="collection basket-list">
       <li className="collection-item active">Корзина</li>
@@ -11,7 +15,9 @@ function BasketList(props) {
       ) : (
         <li className="collection-item">Корзина пуста</li>
       )}
-      <li className="collection-item active">Общая стоимость:</li>
+      <li className="collection-item active">
+        Общая стоимость: {totalPrice} usd.
+      </li>
       <i className="material-icons basket-close" onClick={handleBasketShow}>
         close
       </i>
