fd1724ba88e4c95174bd54d676f7936daa72d5d7
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-06 16:14:49 +0300

    added click on icons remove and add

--- src/components/BasketItem.jsx
+++ src/components/BasketItem.jsx
@@ -12,9 +12,20 @@ function BasketItem(props) {
   return (
     <li className="collection-item">
       {name}
-      <i className="material-icons basket-quantity">remove</i>x{quantity}{' '}
-      <i className="material-icons  basket-quantity">add</i>= {price * quantity}{' '}
-      usd.
+      <i
+        className="material-icons basket-quantity"
+        onClick={() => decQuantity(id)}
+      >
+        remove
+      </i>
+      x{quantity}{' '}
+      <i
+        className="material-icons  basket-quantity"
+        onClick={() => incQuantity(id)}
+      >
+        add
+      </i>
+      = {price * quantity} usd.
       <span class="secondary-content" onClick={() => removeFromBasket(id)}>
         <i class="material-icons basket-delete">close</i>
       </span>
