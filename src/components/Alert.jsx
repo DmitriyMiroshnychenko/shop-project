1e7f45d61460707b5df419548b9a1d78b503adbc
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-08 08:02:41 +0300

    added new component Alert for basket

---
+++ src/components/Alert.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+
+function Alert(props) {
+  const { name } = props;
+
+  useEffect(() => {
+    const timerId = setTimeout();
+  }, [name]);
+  return (
+    <div id="toast-container">
+      <div className="toast">{name} добавлен в корзину</div>
+    </div>
+  );
+}
