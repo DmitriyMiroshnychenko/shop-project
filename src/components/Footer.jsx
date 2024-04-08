3f55b96e03c1f3ace15be6669ac663894391c585
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-08 09:18:28 +0300

    added a link to the site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,12 @@ function Footer() {
       <div className="footer-copyright">
         <div className="container">
           ⓒ{new Date().getFullYear()} Copyright Text by Dmytro Miroshnychenko
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/DmitriyMiroshnychenko/shop-project"
+            rel="noreferrer"
+            target="_blank"
+          >
             Nice
           </a>
         </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,18 @@ function Header() {
   return (
     <nav className="blue darken-1">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="/" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="!#">Nice</a>
+            <a
+              href="https://github.com/DmitriyMiroshnychenko/shop-project"
+              target="_blank"
+              rel="noreferer"
+            >
+              Nice
+            </a>
           </li>
         </ul>
       </div>
