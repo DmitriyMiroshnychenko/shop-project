e205e9fd98f0f016db025a48a2b8344edf4f0e14
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-09 07:13:09 +0300

    Update Context.js

--- src/context.js
+++ src/context.js
@@ -1,11 +1,29 @@
-import { Children, createContext } from 'react';
+import { createContext, useReducer } from 'react';
+import { reducer } from './reducer';
 
 export const ShopContext = createContext();
 
+const initialState = {
+  goods: [],
+  loading: true,
+  order: [],
+  isBasketShow: false,
+  alertName: '',
+};
+
 export const ContextProvider = ({ Children }) => {
-  const value = {
-    example: 'hello from context',
+  const [value, dispatch] = useReducer(reducer, initialState);
+
+  value.closeAlert = () => {
+    dispatch({ type: 'CLOSE_ALERT' });
   };
+
+  value.removeFromBasket = (itemId) => {
+    dispatch({ type: 'REMOVE_FROM_BASKET', payload: { id: itemId } });
+  };
+  //const value = {
+  // example: 'hello from context',
+  //};
 
   return <ShopContext.Provider value={value}>{Children}</ShopContext.Provider>;
 };
