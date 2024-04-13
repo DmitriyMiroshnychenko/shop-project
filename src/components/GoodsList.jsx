52f1d30226023d3dbad73ffaec53274e07e44e95
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-13 21:50:51 +0300

    Update GoodsList

--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -1,7 +1,7 @@
 import { GoodsItem } from './GoodsItem';
 
 function GoodsList(props) {
-  const { goods = [], addToBasket = Function.prototype } = props;
+  const { goods = [] } = props;
 
   if (!goods.length) {
     return <h3>Ooops somthing went wrong</h3>;
@@ -9,7 +9,7 @@ function GoodsList(props) {
   return (
     <div className="goods">
       {goods.map((item) => (
-        <GoodsItem key={item.id} {...item} addToBasket={addToBasket} />
+        <GoodsItem key={item.id} {...item} />
       ))}
     </div>
   );
