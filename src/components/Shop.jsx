ab5962e584bbd07ca946a3250977fc33545b5899
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-13 22:11:09 +0300

    Update Shop.jsx

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,5 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useContext } from 'react';
 import { API_KEY, API_URL } from '../config';
+
+import { ShopContext } from '../context';
 
 import { Preloader } from './Preloader';
 import { GoodsList } from './GoodsList';
@@ -8,53 +10,8 @@ import { BasketList } from './BasketList';
 import { Alert } from './Alert';
 
 function Shop() {
-  const [goods, setGoods] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [order, setOrder] = useState([]);
-  const [isBasketShow, setBasketShow] = useState(false);
-  const [alertName, setAlertName] = useState('');
-
-  const removeFromBasket = (itemId) => {
-    const newOrder = order.filter((el) => el.id !== itemId);
-    setOrder(newOrder);
-  };
-
-  const incQuantity = (itemId) => {
-    const newOrder = order.map((el) => {
-      if (el.id === itemId) {
-        const newQuantity = el.quantity + 1;
-        return {
-          ...el,
-          quantity: newQuantity,
-        };
-      } else {
-        return el;
-      }
-    });
-    setOrder(newOrder);
-  };
-  const decQuantity = (itemId) => {
-    const newOrder = order.map((el) => {
-      if (el.id === itemId) {
-        const newQuantity = el.quantity - 1;
-        return {
-          ...el,
-          quantity: newQuantity >= 0 ? newQuantity : 0,
-        };
-      } else {
-        return el;
-      }
-    });
-    setOrder(newOrder);
-  };
-
-  const handleBasketShow = () => {
-    setBasketShow(!isBasketShow);
-  };
-
-  const closeAlert = () => {
-    setAlertName('');
-  };
+  const { goods, loading, order, isBasketShow, alertName, setGoods } =
+    useContext(ShopContext);
 
   useEffect(function getGoods() {
     fetch(API_URL, {
@@ -64,25 +21,16 @@ function Shop() {
     })
       .then((response) => response.json())
       .then((data) => {
-        data.featured && setGoods(data.featured);
-        setLoading(false);
+        setGoods(data.featured);
       });
   }, []);
 
   return (
     <main className="container content">
-      <Cart quantity={order.length} handleBasketShow={handleBasketShow} />
-      {loading ? <Preloader /> : <GoodsList goods={goods} />}
-      {isBasketShow && (
-        <BasketList
-          order={order}
-          handleBasketShow={handleBasketShow}
-          removeFromBasket={removeFromBasket}
-          incQuantity={incQuantity}
-          decQuantity={decQuantity}
-        />
-      )}
-      {alertName && <Alert name={alertName} closeAlert={closeAlert} />}
+      <Cart quantity={order.length} />
+      {loading ? <Preloader /> : <GoodsList />}
+      {isBasketShow && <BasketList />}
+      {alertName && <Alert />}
     </main>
   );
 }
