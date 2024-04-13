752879448a7a0abbe04faac46a2e212b45454c5e
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-13 21:51:27 +0300

    delete addToBasket

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -14,31 +14,6 @@ function Shop() {
   const [isBasketShow, setBasketShow] = useState(false);
   const [alertName, setAlertName] = useState('');
 
-  const addToBasket = (item) => {
-    const itemIndex = order.findIndex((orderItem) => orderItem.id === item.id);
-
-    if (itemIndex < 0) {
-      const newItem = {
-        ...item,
-        quantity: 1,
-      };
-      setOrder([...order, newItem]);
-    } else {
-      const newOrder = order.map((orderItem, index) => {
-        if (index === itemIndex) {
-          return {
-            ...orderItem,
-            quantity: orderItem.quantity + 1,
-          };
-        } else {
-          return orderItem;
-        }
-      });
-      setOrder(newOrder);
-    }
-    setAlertName(item.name);
-  };
-
   const removeFromBasket = (itemId) => {
     const newOrder = order.filter((el) => el.id !== itemId);
     setOrder(newOrder);
@@ -97,11 +72,7 @@ function Shop() {
   return (
     <main className="container content">
       <Cart quantity={order.length} handleBasketShow={handleBasketShow} />
-      {loading ? (
-        <Preloader />
-      ) : (
-        <GoodsList goods={goods} addToBasket={addToBasket} />
-      )}
+      {loading ? <Preloader /> : <GoodsList goods={goods} />}
       {isBasketShow && (
         <BasketList
           order={order}
