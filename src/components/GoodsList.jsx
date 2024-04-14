04f9da5f2a869e93b50a547ac6f12d7417aa3bc3
Dmytro <88747340+DmitriyMiroshnychenko@users.noreply.github.com>
2024-04-14 21:10:08 +0300

    Update GoodsList

--- src/components/GoodsList.jsx
+++ src/components/GoodsList.jsx
@@ -1,7 +1,9 @@
+import { useContext } from 'react';
+import { ShopContext } from '../context';
 import { GoodsItem } from './GoodsItem';
 
-function GoodsList(props) {
-  const { goods = [] } = props;
+function GoodsList() {
+  const { goods = [] } = useContext(ShopContext);
 
   if (!goods.length) {
     return <h3>Ooops somthing went wrong</h3>;
